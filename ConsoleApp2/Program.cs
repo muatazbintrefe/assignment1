9d0d03eca2e4132483264d43dbb5de032aab1eff
bintrefemuataz <bintrefemuataz@gmail.com>
2026-02-09 19:16:07 +0300

    that was test

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -4,3 +4,4 @@ BILL = Convert.ToDouble(Console.ReadLine());
 if (BILL >= 500) {Console.WriteLine($"this is you bill {BILL = BILL * 0.80} after the disscount %20");}
 else if (BILL >= 300 && BILL <=499){Console.WriteLine($"this is you bill {BILL = BILL * 0.90} after the disscount %10");}
 else{ Console.WriteLine($"this is you bill {BILL}");}
+
